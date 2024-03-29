cce17c785fbdad74389d53402886678eaddff613
lindseyyucha <119362898+lindseyyucha@users.noreply.github.com>
2024-03-29 16:57:39 +0000

    wip

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+const container = document.querySelector('.container');
 
-const dots = document.querySelectorAll('.dot');
+function createDot() {
+  const dot = document.createElement('div');
+  dot.classList.add('dot');
+  container.appendChild(dot);
+}
+
+for (let i = 0; i < 10; i++) {
+  createDot();
+}
+
 let angle = 0;
 
 function moveDots() {
-  dots.forEach(dot => {
-    dot.style.setProperty('--angle', angle + 'deg');
+  const dots = document.querySelectorAll('.dot');
+  dots.forEach((dot, index) => {
+    const dotAngle = angle + (index * (360 / dots.length)); 
+    dot.style.setProperty('--angle', dotAngle + 'deg');
   });
   angle += 1; 
-  requestAnimationFrame(moveDots);
+  requestAnimationFrame(moveDots); 
 }
 
-moveDots();
+moveDots(); 
